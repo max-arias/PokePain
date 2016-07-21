375ac8aa0c2ce04c39e1da6781ad903f0931fb5f
Max Arias <max@max-arias.com>
2016-07-20 21:03:05 -0300

    Fix login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,16 @@
 class SessionsController < ApplicationController
+
+  def new
+    redirect_to :controller => 'articles', :action => 'index' if logged_in?
+  end
+
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       remember user
-      redirect_to :controller => 'admin', :action => 'index'
+      redirect_to :controller => 'articles', :action => 'index'
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
