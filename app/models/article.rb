4c1e2c6aa66d7fc9f5a8ea195d070bae71791a0d
Max Arias <max@max-arias.com>
2016-07-24 17:00:47 -0300

    News date required

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,6 @@ class Article < ApplicationRecord
   attr_accessor :abbr_date
 
   def abbr_date
-    self.abbr_date = date_of_news.strftime("%b. %d")
+    self.abbr_date = date_of_news.strftime("%b. %d") if date_of_news
   end
 end
